6c9af96968649b34bdbca8e80787f9de9433efca
13inuo0 <13inuo0@github.com>
2025-09-02 11:56:25 +0900

    이동 크기 투명도 애니메이션 적용

--- js/animate.js
+++ js/animate.js
@@ -8,4 +8,36 @@ $(function () {
       1000
     );
   });
+  //   크기 변경 애니메이션
+  $(".resize").click(function () {
+    $(".box").animate(
+      {
+        width: "200px",
+        height: "200px",
+      },
+      800
+    );
+  });
+  //   보였다 사라지는 애니메이션
+  $(".fade").click(function () {
+    $(".box").animate(
+      {
+        opacity: ".3",
+      },
+      500
+    );
+  });
+  //   모든 애니메이션
+  $(".all").click(function () {
+    $(".box").animate(
+      {
+        left: "300px",
+        width: "200px",
+        height: "200px",
+        opacity: ".3",
+        borderRadius: "50%",
+      },
+      1000
+    );
+  });
 });
